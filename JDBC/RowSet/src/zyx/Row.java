56180948f345a800d4e6e8d2f64e1c34ec3c5fc3
Vittal777 <93542555+Vittal777@users.noreply.github.com>
2024-02-20 13:08:20 +0530

    Add files via upload

--- JDBC/RowSet/src/zyx/Row.java
+++ JDBC/RowSet/src/zyx/Row.java
@@ -8,37 +8,48 @@ import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
 
 public class Row {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc=new Scanner(System.in);
+		// TODO Auto-generated method stub
 		try {
-		RowSetFactory rsf=RowSetProvider.newFactory();
+			
+		RowSetFactory rsf= RowSetProvider.newFactory();
 		JdbcRowSet jrs=rsf.createJdbcRowSet();
 		jrs.setUrl("jdbc:mysql://localhost:3306/me");
 		jrs.setUsername("root");
 		jrs.setPassword("vittal777");
-		jrs.setCommand("select name,sport,id from sportsmen");
+		/*jrs.setCommand("select * from sportsmen where sport='cricket'");
 		jrs.execute();
-		Scanner sc=new Scanner(System.in);
+		System.out.println("NAME\tSPORT");
+		while(jrs.next()) {
+			System.out.println(jrs.getString(1)+"\t"+jrs.getString(2));
+		}*/
+		jrs.setCommand("select * from sportsmen");
+		jrs.execute();
 		jrs.moveToInsertRow();
 		while(true) {
-			System.out.println("Enter name : ");
-			String name=sc.next();
-			System.out.println("Enter sport : ");
-			String sport=sc.next();
-			jrs.updateString(1, name);
-			jrs.updateString(2, sport);
-			jrs.insertRow();
-			System.out.println("Do you want to insert more rows? [Yes/No]");
-			String choice=sc.next();
-			if(choice.equalsIgnoreCase("no")) {
-				break;
-			}	
+		String name=sc.next();
+		System.out.println("Enter name : ");
+		String sport=sc.next();
+		System.out.println("Enter sport : ");
+		jrs.updateString(1, name);
+		jrs.updateString(2, sport);
+		jrs.insertRow();
+		System.out.println("The row is successfully inserted");
+		System.out.println("Do you want to insert another row? [Yes/No] ");
+		String choice=sc.next();
+		if(choice.equalsIgnoreCase("No")) {
+			break;
 		}
 		sc.close();
 		jrs.close();
-		}catch(SQLException se) {
-			se.printStackTrace();
-		}catch(Exception e) {
-			e.printStackTrace();
 		}
+	}catch(SQLException se) {
+		se.printStackTrace();
+	}catch(Exception e) {
+		e.printStackTrace();
 	}
+	}
+
 }

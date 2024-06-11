2a4a16fac9ea8b0743d9279cedb8fa7a8fca446b
Vittal777 <93542555+Vittal777@users.noreply.github.com>
2024-06-11 13:12:54 +0530

    Delete IntegrationMgmt directory

--- IntegrationMgmt/src/main/java/com/tktservice/ServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.tktservice;
-
-import com.bo.Ticket;
-
-public interface ServiceImpl {
-
-	public String checkSame(String passengerNo);
-	public String generateTicket(Ticket ticket,String pname,String name);
-}
